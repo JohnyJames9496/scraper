e2b099cd2e0db1859fe14d4894e737f556f5af37
johny <johnyjames9496@gmail.com>
2026-02-25 19:33:53 +0530

    database updated

--- app/database/database.py
+++ app/database/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "YOUR_SUPABASE_DATABASE_URL"
+DATABASE_URL = "DATABASE_URL"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
